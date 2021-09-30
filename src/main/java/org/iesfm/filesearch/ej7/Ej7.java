de1cfa8c00781516f46597eaf86be1c7e73f4f94
Ruben <martinillo114@gmail.com>
2021-09-30 13:51:35 +0200

    Create Ej7.java

---
+++ src/main/java/org/iesfm/filesearch/ej7/Ej7.java
@@ -0,0 +1,13 @@
+package org.iesfm.filesearch.ej7;
+
+import org.iesfm.filesearch.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Ej7 {
+    private static Logger log = LoggerFactory.getLogger(Ej7.class);
+
+    private static FileUtils fileUtils = new FileUtils();
+
+
+}
